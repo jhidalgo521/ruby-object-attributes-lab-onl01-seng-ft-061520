1b3bc3886b3a43508b5c17e93d32a745f58062b1
Jose Hidago <jhidalgo521@gmail.com>
2020-06-26 12:21:38 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,42 @@
 class Dog 
+  
   def name= (dogs_name)
-    @dogs_name = name 
+    @name = dogs_name
   end
   
   def name
-    @dogs_name = name 
-  #
+    @name
+  end
+  
+  def breed= (dog_breed)
+    @breed = dog_breed
+  end
+  
+  def breed
+    @breed
+  end 
+  
 end 
 
 class Person
-  #
-  #
-  #
+  #name setter
+  def name= (person_name)
+    @name = person_name
+  end 
+  
+  #name getter
+  def name
+    @name
+  end
+  
+  #job setter
+  def job= (job_title)
+    @job = job_title
+  end
+  
+  #job getter
+  def job 
+    @job
+  end 
+  
 end 
